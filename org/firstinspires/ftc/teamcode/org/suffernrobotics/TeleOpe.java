1464e86b770ef44d0acdeff7c80c0881c5788cd2
bcerra <brian@suffernrobotics.org>
2017-01-25 14:54:31 -0500

    Updated TeleOpe with backup code

--- org/firstinspires/ftc/teamcode/org/suffernrobotics/TeleOpe.java
+++ org/firstinspires/ftc/teamcode/org/suffernrobotics/TeleOpe.java
@@ -2,13 +2,12 @@ package org.firstinspires.ftc.teamcode.org.suffernrobotics;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.GyroSensor;
+import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.Range;
-
-/**
- * Created by robotics1 on 1/13/17.
- */
 
 @TeleOp(name = "TeleOpe", group = "OpModes")
 public class TeleOpe extends LinearOpMode {
@@ -16,10 +15,14 @@ public class TeleOpe extends LinearOpMode {
     DcMotor a, b, c, d, sweep, flick, lift;
     Servo left, right;
 
+    ColorSensor c1;
+    GyroSensor g1;
+    OpticalDistanceSensor o1;
+
     public void initHardware() {
         //Initialize servos
-        left = hardwareMap.servo.get("left");
-        right = hardwareMap.servo.get("right");
+        left = hardwareMap.servo.get("l");
+        right = hardwareMap.servo.get("r");
 
         //Initialize motors
         a = hardwareMap.dcMotor.get("a");
@@ -28,7 +31,11 @@ public class TeleOpe extends LinearOpMode {
         d = hardwareMap.dcMotor.get("d");
         sweep = hardwareMap.dcMotor.get("s");
         flick = hardwareMap.dcMotor.get("f");
-        lift = hardwareMap.dcMotor.get("l");
+        //lift = hardwareMap.dcMotor.get("ll");
+
+        c1 = hardwareMap.colorSensor.get("c1");
+        g1 = hardwareMap.gyroSensor.get("g1");
+        o1 = hardwareMap.opticalDistanceSensor.get("o1");
 
     }
 
@@ -41,18 +48,20 @@ public class TeleOpe extends LinearOpMode {
     @Override
     public void runOpMode(){
         initHardware();
+        double rPos = .5, lPos = 1;
+
         waitForStart();
 
         while(opModeIsActive()){
-            int rPos = 0, lPos = 0;
-            rPos += gamepad2.right_stick_x/50;
+
+            /*rPos += gamepad2.right_stick_x/50;
             lPos += gamepad2.left_stick_x/50;
 
             rPos = Range.clip(rPos, 0, 1);
             lPos = Range.clip(lPos, 0, 1);
 
-            //right.setPosition(1-(rPos*.077));
-            //left.setPosition(.923+(lPos*.077));
+            right.setPosition(1-(rPos*.077));
+            left.setPosition(.923+(lPos*.077));*/
 
             double x = gamepad1.left_stick_x;
             double y = gamepad1.left_stick_y;
@@ -85,27 +94,24 @@ public class TeleOpe extends LinearOpMode {
             }
             if (gamepad1.right_trigger > .75){
                 flick.setPower(-1);
+            } else if(gamepad1.left_trigger > .75){
+                flick.setPower(1);
             }
             else {
                 flick.setPower(0);
             }
 
-            if(gamepad1.x){
-                //left.setDirection(Servo.Direction.FORWARD);
-                //right.setDirection(Servo.Direction.REVERSE);
-                left.setPosition(1);
+            if(gamepad2.x){
+                rPos = .65;
+                lPos = .85;
             }
-            else{
-                //left.setDirection(Servo.Direction.REVERSE);
-                left.setPosition(.85);
-                //right.setDirection(Servo.Direction.FORWARD);
+            if(gamepad2.b){
+                rPos = .5;
+                lPos = 1;
             }
-            if(gamepad1.b){
-                right.setPosition(.85);
-            }
-            else{
-                right.setPosition(1);
-            }
+
+            right.setPosition(Range.clip(rPos, 0, 1));
+            left.setPosition(Range.clip(lPos, 0, 1));
             telemetry.addData("Left", left.getPosition());
             telemetry.addData("Right", right.getPosition());
             telemetry.update();
